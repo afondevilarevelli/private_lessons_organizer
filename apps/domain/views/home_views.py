fbe04215f804e1bc2caafc52d27a76efb3e04c93
afondevilarevelli <antonny.42@gmail.com>
2023-10-15 04:39:41 -0300

    flash messages

--- apps/domain/views/home_views.py
+++ apps/domain/views/home_views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 
 def home(request):
+    messages.info(request, f'asd created for')
+    messages.warning(request, f'Account created for')
+    messages.error(request, f'Account created for')
     return render(request, 'home.html')
 
 
